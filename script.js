ba313db08356b3df20efbeedff39b88c732e5853
dstout22 <davidrstout22@gmail.com>
2026-06-15 15:23:35 -0400

    Completed weather fetch (US restricted)

---
+++ script.js
@@ -0,0 +1,25 @@
+async function getCoordinates(city) {
+    const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json&countryCode=US`);    
+    const data = await response.json();
+    return data;
+}
+
+async function getWeatherData(lattitude, longitude) {
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lattitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`);
+    const data = await response.json();
+    return data;
+}
+
+document.getElementById("GetWeather").addEventListener("click", async () => {
+    const city = document.getElementById("city-input").value;
+    const coordinates = await getCoordinates(city);
+    if (!coordinates.results || coordinates.results.length === 0) {
+    document.getElementById("weather-info").innerHTML = "<p>City not found. Please try again.</p>";
+    return;
+    }
+    const weatherData = await getWeatherData(coordinates.results[0].latitude, coordinates.results[0].longitude);
+    document.getElementById("weather-info").innerHTML = `
+        <p>Temperature: ${weatherData.current.temperature_2m}°C</p>
+        <p>Wind Speed: ${weatherData.current.wind_speed_10m} m/s</p>
+    `;
+});
